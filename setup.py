2817a2bbe28ab15901fbbc3c336c2d7cd77a46fb
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2016-02-01 08:36:37 -0500

    Populate missing parameters in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python
 import scuba.version
 from setuptools import setup
+import os.path
 
 setup(
-    name = 'SCUBA',
+    name = 'scuba',
     version = scuba.version.__version__,
     description = 'Simplify use of Docker containers for building software',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Software Development :: Build Tools',
+    ],
+    license = 'MIT',
+    keywords = 'docker',
     author = 'Jonathon Reinhart',
     author_email = 'jonathon.reinhart@gmail.com',
     url = 'https://github.com/JonathonReinhart/scuba',
