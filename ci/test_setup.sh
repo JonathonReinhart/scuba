aa503a1e03b5145ca89b3e0e5fa006266ec12bd5
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2018-09-11 22:35:58 -0400

    Add test-docker-images/build_all.sh; invoke in CI

--- ci/test_setup.sh
+++ ci/test_setup.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+set -e
+
+topdir=$(cd $(dirname $0)/.. && pwd)
 
 # Pull this image ahead of time, so it's there for the unit tests
 docker pull debian:8.2
+
+# Build docker images for local testing
+$topdir/test-docker-images/build_all.sh
