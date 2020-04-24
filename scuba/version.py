028f38c96cef1ff4fc38e0dede3760046ae0bc88
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2020-04-24 02:48:22 -0400

    Release v2.6.1

--- scuba/version.py
+++ scuba/version.py
@@ -10,7 +10,7 @@ PROJPATH = dirname(PACKAGEPATH)
 DIST_SPEC = 'scuba'
 
 # Base version, which will be augmented with Git information
-BASE_VERSION = '2.6.0'
+BASE_VERSION = '2.6.1'
 
 # This string will be replaced by `git-archive`
 # with the abbreviated commit hash
