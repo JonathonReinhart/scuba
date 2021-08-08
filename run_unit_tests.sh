24b80be44755ebc46dbfeeb136b89e8576b084cf
Jonathon Reinhart <jonathon.reinhart@gmail.com>
2021-08-08 01:50:33 -0400

    test: Create JUnit XML unit test results

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -4,4 +4,4 @@ if [[ $# -ge 1 ]]; then
     what="$@"
 fi
 
-exec python3 -m pytest -v --log-level=DEBUG --cov=scuba --cov=tests $what
+exec python3 -m pytest -v --log-level=DEBUG --junit-xml=unit-test-results.xml --cov=scuba --cov=tests $what
