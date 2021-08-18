d564a5402b4818b3d87639a0a6d245460df73e5a
Jonathon Reinhart <jonathon.reinhart@gmail.com>
2021-08-18 00:19:34 -0400

    docs: Add sphinx.ext.autosectionlabel extension
    This allows us to reference section headings simply by name.

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@ author = 'Jonathon Reinhart'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autosectionlabel',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
